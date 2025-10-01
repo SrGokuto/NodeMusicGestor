4ddff06edf68218976ebc729e16d2f4ab31bdb70
mariana vallderrama <mariana09@gmail.com>
2025-10-01 16:42:07 -0500

    Agregar menu

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,75 @@ import { Usuario } from "./models/Usuario.js";
 import { UsuarioPremium } from "./models/UsuarioPremium.js";
 import { Tema } from "./types/tema.js";
 import { Suscripcion } from "./types/Suscripcion.js";
+import { input, select } from '@inquirer/prompts';
+
+while (true) {
+  const action = await select({
+    message: '¿Qué acción deseas realizar?',
+    choices: [
+      { name: 'Crear Usuario Básico', value: 'crear_basico' },
+      { name: 'Crear Usuario Premium', value: 'crear_premium' },
+      { name: 'Salir', value: 'salir' }
+    ]
+})
+
+  if (action === 'crear_basico') {
+    const id = await input({ message: 'ID del usuario:' });
+    const nombre = await input({ message: 'Nombre del usuario:' });
+    const correo = await input({ message: 'Correo del usuario:' });
+    const fechaRegistroInput = await input({ message: 'Fecha de registro (YYYY-MM-DD):' });
+    const fechaRegistro = new Date(fechaRegistroInput);
+    const usuarioBasico = new Usuario(id, nombre, correo, Suscripcion.GRATUITA, fechaRegistro, 5);
+    console.log('Usuario Básico creado:', usuarioBasico);
+
+  } else if (action === 'crear_premium') {
+    const id = await input({ message: 'ID del usuario:' });
+    const nombre = await input({ message: 'Nombre del usuario:' });
+    const correo = await input({ message: 'Correo del usuario:' });
+    const fechaRegistroInput = await input({ message: 'Fecha de registro (YYYY-MM-DD):' });
+    const fechaRegistro = new Date(fechaRegistroInput);
+    const fechaPagoInput = await input({ message: 'Fecha de pago (YYYY-MM-DD):' });
+    const fechaPago = new Date(fechaPagoInput);
+    const fechaVencimientoInput = await input({ message: 'Fecha de vencimiento (YYYY-MM-DD):' });
+    const fechaVencimiento = new Date(fechaVencimientoInput);
+    const autoRenovacion = await select({
+      message: '¿Deseas activar la auto-renovación?',
+      choices: [
+        { name: 'Sí', value: true },
+        { name: 'No', value: false }
+      ]
+    });
+    const temaPreferido = await select({
+      message: 'Selecciona tu tema preferido:',
+      choices: [
+        { name: 'morado', value: Tema.MORADO },
+        { name: 'verde', value: Tema.VERDE },
+        { name: 'azul', value: Tema.AZUL },
+        { name: 'cian', value: Tema.CIAN }
+      ]
+    });
+    const usuarioPremium = new UsuarioPremium(id, nombre, correo, fechaRegistro, fechaPago, fechaVencimiento, autoRenovacion, temaPreferido);
+    console.log('Usuario Premium creado:', usuarioPremium);
+  } else if (action === 'salir') {
+    console.log('Saliendo del programa.');
+    break;
+
+  }
+  const continuar = await select({
+    message: '¿Deseas realizar otra acción?',
+    choices: [
+      { name: 'Sí', value: true },
+      { name: 'No', value: false }
+    ]
+  });
+  if (!continuar) {
+    console.log('Saliendo del programa.');
+    break;
+  }
+  console.log('Volviendo al menú principal...');
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  console.clear();
+}
+ 
 
 
-const user1 = new Usuario(
-  "001",                        
-  "sara",                 
-  "sara@gmail.com",      
-  Suscripcion.GRATUITA,         
-  new Date("2025-10-01"),      
-  3                             
-);
-
-console.log("=== PRUEBAS DE USUARIO ===");
-console.log(user1.crearPlaylist("Mi Playlist Favorita"));
-console.log(user1.crearPlaylist("Música Relajante"));
-console.log(user1.borrarPlaylist("Mi Playlist Favorita"));
-console.log(user1.borrarPlaylist("Playlist Inexistente"));
-console.log(user1.agregarFavorito("Shape of You"));
-console.log(user1.eliminarFavorito("Shape of You"));
-user1.actualizarSuscripcion(Suscripcion.PREMIUM);
-console.log("Playlists actuales:", user1.getPlaylists());
-
-
-
-const userPremium = new UsuarioPremium(
-  "002",
-  "sara",
-  "sara@gmail.com",
-  new Date("2025-01-01"),   
-  new Date("2025-09-01"),   
-  new Date("2025-10-01"),   
-  true,                     
-  Tema.MORADO               
-);
-
-console.log("\n=== PRUEBAS DE USUARIO PREMIUM ===");
-console.log("Tema actual:", userPremium.getTemaPreferido());
-userPremium.personalizarTema(Tema.CIAN);
-userPremium.toggleRenovacion();
-userPremium.reportarProblema("La música no carga correctamente");
